5db93adc046b454db9ebea2763a8d6ce134de3b1
Aarondsilva0007 <62275952+Aarondsilva0007@users.noreply.github.com>
2022-01-10 11:53:07 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from tensorflow.keras.models import load_model
+from typing import Optional
+import numpy as np
+import json
+from fastapi import FastAPI
+import pickle as pikl
+
+app = FastAPI()
+
+loaded_model = load_model("best_model")
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/predict")
+def scale_predict(text: str):
+    # scaler = pickle.load(open("scaler.pkl", 'rb'))
+    scaler1 = pikl.load(open("scalerlab.pkl", 'rb'))
+    d = json.loads(text)
+    numpy_2d_arrays = np.array(d)
+    # sh = numpy_2d_arrays.shape
+    # arr = np.reshape(numpy_2d_arrays,(1,numpy_2d_arrays.shape[0], numpy_2d_arrays.shape[1]))
+    res = loaded_model.predict(numpy_2d_arrays)
+    # return {"result_ns":f'{res}', "result_s":f'{scaler1.inverse_transform(res)}'}
+    return {"pred": f'{scaler1.inverse_transform(res)[0][0]}', "type": numpy_2d_arrays.shape}
+
+
+@app.get('/Welcome')
+def get_name(name: str):
+    return {'Welcome To Krish Youtube Channel': f'{name}'}
